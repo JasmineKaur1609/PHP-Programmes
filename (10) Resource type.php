97139bc07058ff44442730f92ce2545e93860d2d
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-02-16 16:40:41 +0530

    Update (10) Resource type.php

--- (10) Resource type.php
+++ (10) Resource type.php
@@ -5,5 +5,5 @@
     $handle = fopen("9.txt", "r");
     var_dump($handle);
     echo "<br>";
-    echo "This Program is written by Sarvesh Bhardwaj 0221BCA062"
+    echo "This Program is written by Jasmine Kaur 022BCA123"
 ?>
