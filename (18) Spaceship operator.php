f1869658570ba0bf9dd061ccdca1771a2a92af5c
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-02-16 16:43:01 +0530

    Update (18) Spaceship operator.php

--- (18) Spaceship operator.php
+++ (18) Spaceship operator.php
@@ -23,6 +23,7 @@
     echo "<br>";
     echo "y" <=> "x"; // Outputs: 1
     echo "<br>";
-    echo "This Program is written by Sarvesh Bhardwaj 0221BCA062"
+    echo "This Program is written by Jasmine Kaur 022BCA123
+    "
 
 ?>
