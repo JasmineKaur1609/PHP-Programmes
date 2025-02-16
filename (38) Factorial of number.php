53ca1db5aff05e1f93c68537e510dcb721604ae7
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-02-16 17:07:03 +0530

    Update (38) Factorial of number.php

--- (38) Factorial of number.php
+++ (38) Factorial of number.php
@@ -10,5 +10,5 @@ function factorial($number)
 
 $number = 5;
 echo "The factorial of " . $number . " is " . factorial($number) . "<br>";
-echo"This Program is written by Sarvesh Bhardwaj 0221BCA062"
+echo"This Program is written by Jasmine Kaur 022BCA123"
 ?>
