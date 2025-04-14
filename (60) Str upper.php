0b0fa04cafe66b67654dc77497b0bba7773edb09
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 19:45:45 +0530

    Update (60) Str upper.php

--- (60) Str upper.php
+++ (60) Str upper.php
@@ -3,7 +3,7 @@
 <body>
 <?php
 echo strtoupper("Hello WORLD!");
-echo"<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062"
+echo"<br>This Program is Written by Jasmine KAUR 0221BCA123"
 ?>
 </body>
 </html>
