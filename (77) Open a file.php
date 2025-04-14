976cf56b44b039acddc881944ced378637b30be8
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 20:11:22 +0530

    Update (77) Open a file.php

--- (77) Open a file.php
+++ (77) Open a file.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $file = fopen("Welcome.txt", "r");
 if ($file) {
-    echo "<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062";
+    echo "<br>This Program is Written by Jasmine KAUR 0221BCA123";
     fclose($file);
 } else {
     echo "<br>Failed to open file.";
