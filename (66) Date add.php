1ad891f7f45a861a408efc8dab5a4c2ea18f64d0
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 19:55:15 +0530

    Update (66) Date add.php

--- (66) Date add.php
+++ (66) Date add.php
@@ -5,7 +5,7 @@
 $date = date_create("2025-02-21");
 date_add($date, date_interval_create_from_date_string("40 days"));
 echo date_format($date, "Y-m-d");
-echo "<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062";
+echo "<br>This Program is Written by Jasmine Kaur 0221BCA123";
 ?>
 </body>
 </html>
