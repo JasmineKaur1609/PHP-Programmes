7d628fd677f731e75a5b062792bfa6cd25ae4926
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 20:00:30 +0530

    Update (70) Time function.php

--- (70) Time function.php
+++ (70) Time function.php
@@ -4,5 +4,5 @@ $current_timestamp = time();
 
 // Display the current Unix timestamp
 echo "The current Unix timestamp is: " . $current_timestamp;
-echo "<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062";
+echo "<br>This Program is Written by Jasmine Kaur 0221BCA123";
 ?>
