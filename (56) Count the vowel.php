109449075d796af8281091cd86b72ff42d3ca4df
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 19:41:59 +0530

    Update (56) Count the vowel.php

--- (56) Count the vowel.php
+++ (56) Count the vowel.php
@@ -4,7 +4,7 @@
 
 <?php
 echo str_word_count("Hello World!");
-echo"<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062"
+echo"<br>This Program is Written by Jasmine Kuar 0221BCA123"
 ?>
 </body>
 </html>
