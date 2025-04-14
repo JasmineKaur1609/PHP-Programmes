421488da5d50b69119d53718459a8b638851f5a2
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 19:38:45 +0530

    Update (55) String length .php

--- (55) String length .php
+++ (55) String length .php
@@ -8,7 +8,7 @@
 $my_str = 'Welcome to Tutorial Republic';
 // Calculating and displaying string length
 echo strlen($my_str);
-echo"<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062"
+echo"<br>This Program is Written by Jasmine Kaur 0221BCA123"
 ?>
 </body>
 </html>
