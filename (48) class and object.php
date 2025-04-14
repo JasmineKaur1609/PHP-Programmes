189159bf676f049eeae928339e753d995a0d5666
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 19:24:44 +0530

    Update (48) class and object.php

--- (48) class and object.php
+++ (48) class and object.php
@@ -28,6 +28,6 @@
     
     $rec->showResult();
     
-    echo "<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062";
+    echo "<br>This Program is Written by Jasmine Kaur 0221BCA123";
 
 ?>
