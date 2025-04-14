4c503673e9e47c21b23567d26aa6f962055bc100
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 20:05:08 +0530

    Update (74) Delete cookies.php

--- (74) Delete cookies.php
+++ (74) Delete cookies.php
@@ -2,5 +2,5 @@
 
     // Deleting a cookie
     setcookie("username", "", time()-3600);
-    echo "<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062";
+    echo "<br>This Program is Written by Jasmine Kaur 0221BCA123";
 ?>
