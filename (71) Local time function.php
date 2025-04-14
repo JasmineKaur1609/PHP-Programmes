5edb0427c310bcd06e4890f317294a2f4bf14202
JasmineKaur1609 <jasminekaur844@outlook.com>
2025-04-14 20:01:20 +0530

    Update (71) Local time function.php

--- (71) Local time function.php
+++ (71) Local time function.php
@@ -4,5 +4,5 @@ $local_time = date('Y-m-d H:i:s');
 
 // Display the local time
 echo "The current local time is: " . $local_time;
-echo "<br>This Program is Written by Sarvesh Bhardwaj 0221BCA062";
+echo "<br>This Program is Written by Jasmine Kaur 0221BCA123";
 ?>
